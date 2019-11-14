db80beed986290963774866453d5f49cbbdd2041
DorotaDAB <dorota.dabrowska.kowalska@gmail.com>
2019-11-14 23:11:56 +0100

    Offers: component layout prepared

--- src/Components/Offers/Offers.js
+++ src/Components/Offers/Offers.js
@@ -2,10 +2,32 @@ import React from 'react';
 import './Offers.css';
 
 class Offers extends React.Component {  
+  
+  calculateLeaseRate() {
+    alert('W przygotowaniu');
+  }
+
 	render() {
     return (
       <div className="offers">
-        <h3>Oferty: w przygotowaniu</h3>
+        <p>Masz już pojazd? Sfinansujemy go dla Ciebie!</p>
+        <form className="input-form">
+          <label for="term">Liczba rat (24-60)</label>
+          <input className="input" type="number" label="value" min="24" max="60.000" step="1"></input>
+        </form>
+        <form className="input-form">
+          <label for="value">Podaj wartosc pojazdu:</label>
+          <input className="input" type="number" label="value" min="0" max="500000" step="1"></input>
+        </form>
+        <form className="input-form">
+          <label for="contribution">Opłata wstępna (1-40%):</label>
+          <input className="input" type="number" label="contribution" min="0" max="40.000" step="1"></input>
+        </form>
+        <form className="input-form">
+          <label for="residual">Wartosć wykupu (1-40%):</label>
+          <input type="number" label="residual" min="0" max="40.000" step="1"></input>
+        </form>
+        <button onClick={this.calculateLeaseRate}>Oblicz ratę</button>
       </div>
     )}
 };
